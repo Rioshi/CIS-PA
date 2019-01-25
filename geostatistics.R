f0d584edbfd1b81a5d0889be9684e1c7860c5af5
juliancmn <juliancmn@users.noreply.github.com>
2019-01-24 20:19:52 -0500

    Update geostatistics.R

--- geostatistics.R
+++ geostatistics.R
@@ -74,7 +74,7 @@ plot(nn)
 library(gstat)
 gs <- gstat(formula=pH~1, locations=ap.sp)
 idw <- interpolate(ap.rast2, gs)
-plot(idw)
+plot(idw,main="pH del suelo")
 
 gs <- gstat(formula=pH~1, locations=ap.sp, nmax=5, set=list(idp=1))
 idw <- interpolate(ap.rast2, gs)
@@ -98,29 +98,36 @@ plot(idw)
 
 #Variograma
 library(gstat)
-variog <- variogram(pH~1, ap.sp)
+variog <- variogram(CEc~1, ap.sp)
 plot(variog)
 
 #cutoff = distancia máxima a la que queremos sacar la semivarianza
 #width = intervalo entre las distancias
-variog <- variogram(pH~1, ap.sp, width=3,cutoff=30)
+variog <- variogram(pH~1, ap.sp, width=6,cutoff=40)
 plot(variog)
 
 #variograma ajustado
-variog.ajust <- fit.variogram(variog, model=vgm(psill=var(ap.sp$pH), 
-                                                model="Gau", range=5, nugget=0),fit.ranges = FALSE)
+variog.ajust <- fit.variogram(variog, model=vgm(psill=var(ap.sp$CEc), 
+                                                model="Gau", range=15, nugget=0.10),fit.ranges = FALSE, fit.method = 7)
 #psill: Semivarianza maxima (donde ya no hay autocorrelacion) = varianza
 #range: Distancia a la cual la semivarianza llega al maximo
 #nugget: Distancia donde comienza a existir autocorrelacion espacial
 plot(variog, variog.ajust)
+ap.rast2 <- raster(ncol=24, nrow=12, xmn=-2, xmx=46, ymn=-2, ymx=22)
+ap.rast2
+values(ap.rast2) <- 1 #agregar valores de 0 a cada celda
+crs(ap.rast2) <- "+proj=utm +zone=18 +south +ellps=WGS84 +datum=WGS84 +units=m +no_defs" #asignar sistema de coordenadas
+plot(ap.rast2)
 
 #Kriging
 nl <- as(ap.rast2, 'SpatialGridDataFrame')
-pH.krige <- krige(formula=pH~1,locations = ap.sp,newdata=nl) 
+pH.krige <- krige(formula=pH~1,locations = ap.sp,newdata=nl,model=variog.ajust) 
 #newdata debe ser un Grid (no puede ser raster)
 pH.plo <- raster(pH.krige["var1.pred"])
-plot(pH.plo)
+plot(pH.plo,main="Conductividad eléctrica del suelo (1:1)")
 
+spplot(pH.krige["var1.pred"], main="Residuos krigeados (kriging ordinario)")
+spplot(ap.sp["pH"])
 #####################
 ## COMPARACION GRAFICA
 #####################
